a7157c1e4f2553c23ca6e7e919481cd151cec7e1
DESKTOP-C7OLCFB\Jesus <jihernandezr@gmail.com>
2017-04-11 19:52:29 +0200

    He añadido varios holas

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public static void main(String []args){
 	System.out.print("Hola!");
 System.out.print("Hola Miguel Angel!");
+System.out.print("Hola Jesus!");
+System.out.print("Hola Jose Luis!");
 
 }
