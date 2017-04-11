1775b0c566519c49eddd568cf15caddd33b6179c
DESKTOP-C7OLCFB\Jesus <jihernandezr@gmail.com>
2017-04-11 20:11:37 +0200

    Cambios de miguel angel y Jesu

--- Main.java
+++ Main.java
@@ -11,11 +11,6 @@ public static void main(String []args){
 System.out.print("Hola Miguel Angel!");
 System.out.print("Hola Jesus!");
 System.out.print("Hola Jose Luis!");
-<<<<<<< HEAD
 System.out.print("Adios Jose Luis!");
-=======
 
-}
-
->>>>>>> commit
 }
