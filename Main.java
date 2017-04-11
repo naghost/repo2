6dc7dd5fd349e297b901f3b96a8495757cc694c0
DESKTOP-C7OLCFB\Jesus <jihernandezr@gmail.com>
2017-04-11 20:02:05 +0200

    commit

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
+
+
+class Test{
+
+public void adios(){
+	System.out.println("adios");
+}
+
 public static void main(String []args){
 	System.out.print("Hola!");
 System.out.print("Hola Miguel Angel!");
 System.out.print("Hola Jesus!");
 System.out.print("Hola Jose Luis!");
+
+}
 
 }
